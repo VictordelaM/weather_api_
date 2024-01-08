096e196ef4f8d6f4a3b316a2acb61c61105b3ca9
VictordalaM <delamotte.v@gamil.com>
2024-01-09 00:19:30 +0100

    clear Interval fix & comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
+//! counter damit die clearIntervalfunktion erst nach dem ersten durchlauf aufgerufen wird
+let counter = 0   
+
+
+//!definition der onClick Funktion
 const check = () => {
-    clearInterval()
+
+
+ //! HTML Elemente
     let input = document.getElementById('input').value
-    let cityOutput = document.getElementById('cityName')
+    let cityOutput = document.getElementById('cityName')                           
     let countryOutput = document.getElementById('countryCodeBox')
     let weatherIconOutput = document.getElementById('icon')
     let tempOutput = document.getElementById('temperatur')
@@ -15,36 +22,30 @@ const check = () => {
     let coordyOutput = document.getElementById('coordsBox')
 
 
-
-    const reset = () => {
-        weatherIconOutput.innerHTML = ""
-        cityOutput.innerHTML = ""
-        countryOutput.innerHTML = ""
-        tempOutput.innerHTML = ""
-        timeOutput.innerHTML = ""
-        windOutput.innerHTML = ""
-        cloudsOutput.innerHTML = ""
-        pressureOutput.innerHTML = ""  
-        humitidyOutput.innerHTML =""
-        sunriseOutput.innerHTML = ""
-        sunsetOutput.innerHTML = ""
-        coordyOutput.innerHTML = ""
-    }
-    reset()
-
-
+//! fetch für Wetterdaten aus API
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4bcea2e105b56117dc4f5cdd3a2391fd`)
 .then((response) => response.json())
-.then((data) => {
-        setInterval(() => {
+.then((data) => {                                    
+
+
+        //! aufruf der clearInterval Funktion erst nach dem ersten Durchlauf
+        if(counter > 0){clearInterval(refresh)}
+
+
+        //!Variable für Zeit in setInterval Methode alle 1000 millisekunden um jede sekunde zu aktuallisieren
+        refresh = setInterval(() => {                   //? wenn als variable definiert funktioniert es nicht
             let currentdate = new Date()
             let seconds = currentdate.getTime()
             let time = new Date(seconds-3600000+data.timezone*1000).toLocaleTimeString()
             timeOutput.innerHTML = `<p>${time}</p>`
         },1000)
-        // let currentdate = new Date()
-        // let seconds = currentdate.getTime()
-        // let time = new Date(seconds-3600000+data.timezone*1000).toLocaleTimeString()
+
+
+        //! nach dem ersten aufruf dieser Operation wird die clearInterval funktion aufgerufen
+        counter += 1    
+
+
+        //! variablen in denen die Wetterdaten gespeichert werden 
         let weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
         let temp = Math.round(data.main.temp - 273.15) + "°"
         let windSpeed = `${data.wind.speed} (${data.wind.deg})`
@@ -59,12 +60,11 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4bcea2e1
         let country = data.sys.country
 
 
-
+        //! Einfügen der entsprechenden Wetterdaten in der HTML
         weatherIconOutput.setAttribute("src", weatherIcon)
         cityOutput.innerHTML = `<p>${city}</p>`
         countryOutput.innerHTML = `<p>${country}</p>`
         tempOutput.innerHTML = `<p>${temp}</p>`
-        // timeOutput.innerHTML = `<p>${time}</p>`
         windOutput.innerHTML = `<p>${windSpeed}</p>`
         cloudsOutput.innerHTML = `<p>${clouds}</p>`
         pressureOutput.innerHTML = `<p>${pressure}</p>`    
@@ -72,8 +72,5 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=4bcea2e1
         sunriseOutput.innerHTML = `<p>${sunrise}</p>`
         sunsetOutput.innerHTML = `<p>${sunset}</p>`
         coordyOutput.innerHTML = `<p>${coordyX}, ${coordyY}</p>`
-
-        console.log( new Date(data.sys.sunrise));
-        console.log( new Date(data.sys.sunset));
 })
 }
